d8d886d293cdd3a763f4dcb040bfa191f9e650fc
Mihail83 <Sklif83@tut.by>
2021-03-01 16:50:11 +0300

    Add project files.

---
+++ OrderManagmentApp.DataLayer/EntityModels/OrderEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OrderManagmentApp.DataLayer.EntityModels
+{
+    public class OrderEntity
+    {
+  
+
+    }
+}
